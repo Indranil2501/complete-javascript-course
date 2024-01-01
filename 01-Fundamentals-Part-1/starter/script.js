ec4d164c10b5b4035e2a7c21b6fe41cc49d683ac
Indranil <Indranil2501@users.noreply.github.com>
2024-01-01 20:31:07 +0530

    Update script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,10 @@
-let js = 'amaizing';
+/*
+////////////////////////////////////
+// Linking a JavaScript File
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
 if (js === 'amaizing') alert ("JavaScript ROCKS!!!")
 console.log(typeof true) //Output: boolean
 console.log(typeof null);
+
